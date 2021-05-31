80e8ed369c3c61c09d33634692f876ac1e8f5bde
unknown <b31@mylangara.ca>
2021-05-31 02:12:26 -0700

    first commit

---
+++ js/style.js
@@ -0,0 +1,19 @@
+
+
+// Navigation scroll shadow
+$(window).scroll(function() {
+
+    var scrolling = $(window).scrollTop();
+
+    if(scrolling > 50) {
+        $("header").addClass("shadow");
+    } else {
+        $("header").removeClass("shadow");
+    }
+});
+
+
+$(".nav_container").click(function(){
+    alert("in nav");
+    $(".navigation").toggleClass("resp");
+});
